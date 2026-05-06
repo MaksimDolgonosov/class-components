6c0ae07f7b1ff37c6108f8fff4d16b59c865e7e4
Maksim Dolgonosov <max_air@bk.ru>
2026-05-06 15:52:54 +0300

    feat: add testing of component App.tsx and updated Husky to run tests on the pre-push hook

--- __tests__/test-utils/App.test.tsx
+++ __tests__/test-utils/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import App from '../../src/components/App/App';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import getPokemonListWithDescription from '../../src/services/fetchPokemons';
-import { PokemonDescription } from '../../src/types/types';
+
 
 vi.mock('../../src/services/fetchPokemons', () => ({
   default: vi.fn().mockResolvedValue({
@@ -15,6 +14,10 @@ vi.mock('../../src/services/fetchPokemons', () => ({
 }));
 
 describe('App', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('renders', () => {
     const { getByText, getByRole } = render(<App />);
 
@@ -26,10 +29,10 @@ describe('App', () => {
 
   it('shows loading state when loading is true', async () => {
     vi.mock('../../src/services/fetchPokemons', () => ({
-      default: vi.fn(() => new Promise(() => {})), // не резолвится
+      default: vi.fn(() => new Promise(() => { })), // не резолвится
     }));
-    const { getByAltText } = render(<App />);
-    expect(getByAltText('spinner')).toBeDefined();
+    render(<App />);
+    expect(screen.getByAltText('spinner')).toBeDefined();
   });
 
   it('sets pokemon from localStorage to state on mount', async () => {
@@ -66,13 +69,25 @@ describe('App', () => {
 
   it('shows error message when API returns error', async () => {
     vi.mocked(getPokemonListWithDescription).mockRejectedValue(
-      new Error('API error')
+      new Error('API error, status: 404')
     );
     render(<App />);
     await waitFor(() => {
-      expect(screen.getByText(/Server error: API error/)).toBeDefined();
+      expect(screen.getByText(/Server error: API error, status: 404/)).toBeDefined();
     });
   });
+
+  it('sets search value to localStorage when search is clicked', async () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText<HTMLInputElement>('Search for a pokemon');
+    fireEvent.change(input, { target: { value: 'pikachu2' } });
+    fireEvent.click(screen.getByRole('button', { name: 'Search' }));
+    await waitFor(() => {
+      expect(globalThis.localStorage.setItem).toHaveBeenCalledWith('pokemon', 'pikachu2');
+      expect(input.value).toBe('pikachu2');
+    });
+  })
+
 
   it('does not write to localStorage when search value is unchanged', async () => {
     vi.mocked(globalThis.localStorage.getItem).mockReturnValue('Pikachu');
@@ -88,4 +103,69 @@ describe('App', () => {
     expect(setItemSpy).not.toHaveBeenCalledWith('pokemon', 'Pikachu');
     setItemSpy.mockRestore();
   });
+
+
+
+  it('changes page using next link and renders new results', async () => {
+    vi.mocked(getPokemonListWithDescription)
+      .mockResolvedValueOnce({
+      results: [
+        {
+          name: 'pikachu',
+          description: 'Electric mouse',
+          imageUrl: 'pikachu.png',
+        },
+      ],
+      next: 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=10',
+      previous: null,
+      errorMessage: null,
+      })
+      .mockResolvedValueOnce({
+        results: [
+          {
+            name: 'bulbasaur',
+            description: 'Seed pokemon',
+            imageUrl: 'bulbasaur.png',
+          },
+        ],
+        next: null,
+        previous: 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0',
+        errorMessage: null,
+      });
+
+    render(<App />);
+
+    const previousButton = screen.getByRole<HTMLButtonElement>('button', {
+      name: 'Previous page',
+    });
+    const nextButton = screen.getByRole<HTMLButtonElement>('button', {
+      name: 'Next page',
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText('pikachu')).toBeDefined();
+      expect(previousButton).toBeDisabled();
+      expect(nextButton).toBeEnabled();
+    });
+
+    fireEvent.click(nextButton);
+
+    await waitFor(() => {
+      expect(getPokemonListWithDescription).toHaveBeenNthCalledWith(
+        2,
+        'https://pokeapi.co/api/v2/pokemon?limit=10&offset=10'
+      );
+      expect(screen.getByText('bulbasaur')).toBeDefined();
+    });
+  });
+
+
+  it('shows errorBoundary message ', async () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole('button', { name: 'Error test' }));
+    await waitFor(() => {
+      expect(screen.getByText('Something went wrong. Please try again later.')).toBeDefined();
+    });
+  });
+
 });
