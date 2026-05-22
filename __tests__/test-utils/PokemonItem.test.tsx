047104f1393235714c720347e458fbafbebd48db
Maksim Dolgonosov <max_air@bk.ru>
2026-05-22 11:44:59 +0300

    fix: fix lint warnings

--- __tests__/test-utils/PokemonItem.test.tsx
+++ __tests__/test-utils/PokemonItem.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { waitFor } from '@testing-library/react';
 import PokemonItem from '../../src/components/PokemonItem/PokemonItem';
